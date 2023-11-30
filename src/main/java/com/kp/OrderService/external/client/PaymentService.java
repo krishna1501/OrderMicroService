2b14dc5ea7fca1876c9c6f5f38d4319b9645f4d3
B V K Prasanth <krishnaprasanth.bv@gmail.com>
2023-11-30 06:56:42 -0500

    Implemented rate limiting and circuit breaker to the microservices

--- src/main/java/com/kp/OrderService/external/client/PaymentService.java
+++ src/main/java/com/kp/OrderService/external/client/PaymentService.java
@@ -1,14 +1,22 @@
 package com.kp.OrderService.external.client;
 
+import com.kp.OrderService.exception.OrderServiceCustomException;
 import com.kp.OrderService.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 	@PostMapping("/doPayment")
 	ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+	default void fallback(Exception e) {
+		throw new OrderServiceCustomException("Payment Service is not available!", "UNAVAILABLE", 500);
+	}
+
 
 }
--- src/main/java/com/kp/OrderService/external/client/ProductService.java
+++ src/main/java/com/kp/OrderService/external/client/ProductService.java
@@ -1,13 +1,20 @@
 package com.kp.OrderService.external.client;
 
+import com.kp.OrderService.exception.OrderServiceCustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/products")
 public interface ProductService {
 	@PutMapping("/reduceQuantity/{id}")
 	ResponseEntity<Void> reduceProducts(@PathVariable("id") long productId, @RequestParam long quantity);
+
+	default void fallback(Exception e) {
+		throw new OrderServiceCustomException("Payment Service is not available!", "UNAVAILABLE", 500);
+	}
 }
